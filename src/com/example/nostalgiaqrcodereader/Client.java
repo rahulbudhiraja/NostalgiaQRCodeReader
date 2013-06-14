795890efe0f40dce962fee9f5a3757de3c57dafe
Rahul Budhiraja <rahul.budhiraja.dark@gmail.com>
2013-06-14 03:38:18 -0400

    Nostalgia QR code app

---
+++ src/com/example/nostalgiaqrcodereader/Client.java
@@ -0,0 +1,55 @@
+package com.example.nostalgiaqrcodereader;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.DatagramPacket;
+
+ 
+import android.util.Log;
+ 
+public class Client implements Runnable {
+        
+      private static byte[]receivedata=new byte[1024];
+      //private static Datagrampacket recv_packet;  
+      private static String sendData;
+      private static String ipAddress;
+       
+      Client(String str,String ip)
+      {
+        sendData=str;
+        ipAddress=ip;
+        
+        Log.d("Sending",sendData+"");
+      }
+      
+        public void run() {
+                try {
+                        // Retrieve the ServerName
+                        InetAddress serverAddr = InetAddress.getByName(ipAddress);
+                        
+                        Log.d("UDP", "C: Connecting...");
+                        /* Create new UDP-Socket */
+                        DatagramSocket socket = new DatagramSocket();
+                       
+                        /* Prepare some data to be sent. */
+                        byte[] buf = sendData.getBytes();
+                       
+                        /* Create UDP-packet with
+                         * data & destination(url+port) */
+                        DatagramPacket packet = new DatagramPacket(buf,buf.length,serverAddr,12001);
+                        Log.d("UDP", "C: Sending: '" + new String(buf) + "'");
+                       
+                        /* Send out the packet */
+                                             
+                      socket.send(packet);
+                        
+                        Log.d("UDP", packet.getData().toString());
+                    
+                        
+                        //Log.d("UDP", "C: Done.");
+                } catch (Exception e) {
+                        Log.e("UDP", "C: Error", e);
+                }
+        }
+}
---
+++ src/com/example/nostalgiaqrcodereader/NostalgiaQRCode.java
@@ -0,0 +1,94 @@
+package com.example.nostalgiaqrcodereader;
+
+
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+
+public class NostalgiaQRCode extends Activity {
+
+	Intent intent;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_nostalgia_qrcode);
+		
+		 intent = new Intent("com.google.zxing.client.android.SCAN");
+		 intent.setPackage("com.google.zxing.client.android");
+		 intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+		intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE", "QR_CODE_MODE");
+		intent.putExtra("SCAN_WIDTH", 1280);
+		intent.putExtra("SCAN_HEIGHT", 768);
+		intent.putExtra("SAVE_HISTORY",false);
+		startActivityForResult(intent, 0);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.nostalgia_qrcode, menu);
+		return true;
+	}
+	public void scanNow(View view)
+	{
+		Log.d("test", "button works!");
+		
+		List<PackageInfo> packagess = getPackageManager().getInstalledPackages(0);
+		for(int i=0;i<packagess.size();i++) {
+		    PackageInfo pack = packagess.get(i);
+		    if ((pack.versionName == null)) {
+		        continue ;
+		    }
+
+		  //this is the application name
+		    Log.d("NAme: ",pack.applicationInfo.loadLabel(getPackageManager()).toString());
+
+		  //this is the package name
+		    Log.d("Package name",pack.packageName);
+		    
+
+		}
+		
+//		Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+//		 intent.setPackage("com.google.zxing.client.android");
+//		 intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+//		intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE", "QR_CODE_MODE");
+//		intent.putExtra("SCAN_WIDTH", 1280);
+//		intent.putExtra("SCAN_HEIGHT", 768);
+//		intent.putExtra("SAVE_HISTORY",false);
+//		startActivityForResult(intent, 0);
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) 
+	{
+		Log.d("Passing","Once");
+	    if (requestCode == 0) 
+	    {
+	        if (resultCode == RESULT_OK)
+	        {
+	            String contents = intent.getStringExtra("SCAN_RESULT");
+	            String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+	            Log.i("xZing", "contents: "+contents+" format: "+format);
+	           
+	            new Thread(new Client(contents,"rahul-macbook-pro.media.mit.edu")).start();// Handle successful scan
+	           // new Thread(new Client(contents,"18.85.58.122")).start();
+	            
+	        	startActivityForResult(intent, 0);
+	            
+	        } 
+	        else if (resultCode == RESULT_CANCELED)
+	        {
+	            // Handle cancel
+	            Log.i("xZing", "Cancelled");
+	        }
+	    }
+	}
+	
+}
